45e95bf5a49beb2392ed93edcc73e60c009b0b18
ilyakhd <ilyakhd@users.noreply.github.com>
2019-03-15 12:08:22 +0300

    Implement block boundaries movement

--- blocksProcessing/file-blocks-tracker.test.ts
+++ blocksProcessing/file-blocks-tracker.test.ts
@@ -1,5 +1,12 @@
 import { IBlock } from "./block";
-import { IChunk, FileBlocksTracker, ILinesDelta } from "./file-blocks-tracker";
+import {
+    IChunk,
+    FileBlocksTracker,
+    IBlockChange,
+    countBlockBalance,
+    findNewBlockStart,
+    findNewBlockEnd
+} from "./file-blocks-tracker";
 
 describe("file-blocks-tracker", () => {
 
@@ -42,6 +49,253 @@ describe("file-blocks-tracker", () => {
             }]);
     });
 
+    describe("block moved boundaries detection", () => {
+
+        it("counts block balance", () => {
+            expect(countBlockBalance("{asdaasd}{sdsd")).toBe(1);
+            expect(countBlockBalance("{asdaasd}{sds{d{")).toBe(3);
+            expect(countBlockBalance("{asdaasd}{sds{d{}}}}}")).toBe(-2);
+            expect(countBlockBalance("{asdaasd}}{sds{d{}}}")).toBe(-1);
+        });
+
+        describe("block start", () => {
+
+            it("finds block new start (first -> first)", () => {
+                const newBlockStartLineIndex = findNewBlockStart(
+                    [
+                        "var t = { a: function() {",
+                        "const y = 1;"
+                    ],
+                    [
+                        "a: function() {",
+                        "const y = 2;"
+                    ],
+                    0
+                );
+
+                expect(newBlockStartLineIndex).toBe(0);
+            });
+
+            it("finds block new start (first -> center)", () => {
+                const newBlockStartLineIndex = findNewBlockStart(
+                    [
+                        "var t = { a: function() {",
+                        "const y = 1;"
+                    ],
+                    [
+                        "const x1 = 1;",
+                        "const x2 = 1;",
+                        "const x3 = 1;",
+                        "var t = { ",
+                        "a: function() {",
+                        "const y = 2;"
+                    ],
+                    0
+                );
+
+                expect(newBlockStartLineIndex).toBe(4);
+            });
+
+            it("finds block new start (first -> last)", () => {
+                const newBlockStartLineIndex = findNewBlockStart(
+                    [
+                        "var t = { a: function() {",
+                        "const y = 1;"
+                    ],
+                    [
+                        "const x1 = 1;",
+                        "const x2 = 1;",
+                        "const x3 = 1;",
+                        "var t = { ",
+                        "a: function() {"
+                    ],
+                    0
+                );
+
+                expect(newBlockStartLineIndex).toBe(4);
+            });
+
+            it("finds block new start (center -> center)", () => {
+                const newBlockStartLineIndex = findNewBlockStart(
+                    [
+                        "const x = 1;",
+                        "var t = { a: function() {",
+                        "const y = 1;"
+                    ],
+                    [
+                        "const x1 = 1;",
+                        "const x2 = 1;",
+                        "const x3 = 1;",
+                        "var t = { ",
+                        "a: function() {",
+                        "const y = 2;"
+                    ],
+                    1
+                );
+
+                expect(newBlockStartLineIndex).toBe(4);
+            });
+
+            it("finds block new start (center -> last)", () => {
+                const newBlockStartLineIndex = findNewBlockStart(
+                    [
+                        "const x = 1;",
+                        "var t = { a: function() {",
+                        "const y = 1;"
+                    ],
+                    [
+                        "const x1 = 1;",
+                        "const x2 = 1;",
+                        "const x3 = 1;",
+                        "var t = { ",
+                        "a: function() {"
+                    ],
+                    1
+                );
+
+                expect(newBlockStartLineIndex).toBe(4);
+            });
+
+            it("finds block new start (last -> last)", () => {
+                const newBlockStartLineIndex = findNewBlockStart(
+                    [
+                        "const x = 1;",
+                        "var t = { a: function() {"
+                    ],
+                    [
+                        "const x1 = 1;",
+                        "const x2 = 1;",
+                        "const x3 = 1;",
+                        "var t = { ",
+                        "a: function() {"
+                    ],
+                    1
+                );
+
+                expect(newBlockStartLineIndex).toBe(4);
+            });
+        });
+
+        describe("block end", () => {
+
+            it("finds block new end (first -> first)", () => {
+                const newBlockEndLineIndex = findNewBlockEnd(
+                    [
+                        "}, a: 1}",
+                        "const y = 1;"
+                    ],
+                    [
+                        "},",
+                        "a: 1}"
+                    ],
+                    0
+                );
+
+                expect(newBlockEndLineIndex).toBe(0);
+            });
+
+            it("finds block new end (first -> center)", () => {
+                const newBlockEndLineIndex = findNewBlockEnd(
+                    [
+                        "}, a: 1}",
+                        "const y = 1;"
+                    ],
+                    [
+                        "const x1 = 1;",
+                        "const x2 = 1;",
+                        "const x3 = 1;",
+                        "},",
+                        "a: 1}",
+                        "const y = 2;"
+                    ],
+                    0
+                );
+
+                expect(newBlockEndLineIndex).toBe(3);
+            });
+
+            it("finds block new end (first -> last)", () => {
+                const newBlockEndLineIndex = findNewBlockEnd(
+                    [
+                        "}, a: 1}",
+                        "const y = 1;"
+                    ],
+                    [
+                        "const x1 = 1;",
+                        "const x2 = 1;",
+                        "const x3 = 1;",
+                        "},",
+                        "a: 1}"
+                    ],
+                    0
+                );
+
+                expect(newBlockEndLineIndex).toBe(3);
+            });
+
+            it("finds block new end (center -> center)", () => {
+                const newBlockEndLineIndex = findNewBlockEnd(
+                    [
+                        "const x = 1;",
+                        "}, a: 1}",
+                        "const y = 1;"
+                    ],
+                    [
+                        "const x1 = 1;",
+                        "const x2 = 1;",
+                        "const x3 = 1;",
+                        "},",
+                        "a: 1}",
+                        "const y = 2;"
+                    ],
+                    1
+                );
+
+                expect(newBlockEndLineIndex).toBe(3);
+            });
+
+            it("finds block new end (center -> last)", () => {
+                const newBlockEndLineIndex = findNewBlockEnd(
+                    [
+                        "const x = 1;",
+                        "}, a: 1}",
+                        "const y = 1;"
+                    ],
+                    [
+                        "const x1 = 1;",
+                        "const x2 = 1;",
+                        "const x3 = 1;",
+                        "},",
+                        "a: 1}"
+                    ],
+                    1
+                );
+
+                expect(newBlockEndLineIndex).toBe(3);
+            });
+
+            it("finds block new end (last -> last)", () => {
+                const newBlockEndLineIndex = findNewBlockEnd(
+                    [
+                        "const x = 1;",
+                        "}, a: 1}"
+                    ],
+                    [
+                        "const x1 = 1;",
+                        "const x2 = 1;",
+                        "const x3 = 1;",
+                        "},",
+                        "a: 1}"
+                    ],
+                    1
+                );
+
+                expect(newBlockEndLineIndex).toBe(3);
+            });
+        });
+
+    });
+
     describe("chunks processing", () => {
 
         it("updates block start accroding to chunk newStart", () => {
@@ -62,9 +316,9 @@ describe("file-blocks-tracker", () => {
                 }
             ];
 
-            const actual = new FileBlocksTracker(blocks).splitChunks(chunks);
+            const actual = new FileBlocksTracker(blocks).applyChunks(chunks);
 
-            const expected: ILinesDelta[] = [
+            const expected: IBlockChange[] = [
                 {
                     startDelta: 100,
                     endDelta: 0
@@ -92,43 +346,62 @@ describe("file-blocks-tracker", () => {
                 }
             ];
 
-            const actual = new FileBlocksTracker(blocks).splitChunks(chunks);
+            const actual = new FileBlocksTracker(blocks).applyChunks(chunks);
 
             expect(actual).toEqual([]);
         });
 
-        // it("detects moved block start", () => {
-        //     const blocks: IBlock[] = [
-        //         {
-        //             name: undefined,
-        //             startLine: 8,
-        //             endLine: 12
-        //         }
-        //     ];
+        it("detects moved block start", () => {
+            const blocks: IBlock[] = [
+                {
+                    name: undefined,
+                    startLine: 8,
+                    endLine: 12
+                }
+            ];
 
-        //     const chunks: IChunk[] = [
-        //         {
-        //             start: 4,
-        //             prevLength: 7,
-        //             newLength: 9
-        //         }
-        //     ];
+            const deletedLines = [
+                "const x1 = 1;",
+                "const x2 = 1;",
+                "const x3 = 1;",
+                "const x4 = 1;",
+                "var t = { a: function() {",
+                "const y1 = 1;",
+                "const y2 = 1;"
+            ];
 
-        //     const actual = new FileBlocksTracker(blocks).splitChunks(chunks);
+            const addedLines = [
+                "const b1 = 1;",
+                "const b2 = 1;",
+                "var t = { a: function() {",
+                "const z1 = 1;",
+                "const z2 = 1;",
+                "const z3 = 1;",
+                "const z4 = 1;",
+                "const z5 = 1;",
+                "const z6 = 1;"
+            ];
 
-        //     const expected: ILinesDelta[] = [
-        //         {
-        //             start: 4,
-        //             delta: 4
-        //         },
-        //         {
-        //             start: 8,
-        //             delta: +2
-        //         }
-        //     ];
+            const chunks: IChunk[] = [
+                {
+                    start: 4,
+                    newStart: 4,
+                    deletedLines,
+                    addedLines
+                }
+            ];
 
-        //     expect(actual).toEqual(expected);
-        // });
+            const actual = new FileBlocksTracker(blocks).applyChunks(chunks);
+
+            const expected: IBlockChange[] = [
+                {
+                    startDelta: -2,
+                    endDelta: 4
+                }
+            ];
+
+            expect(actual).toEqual(expected);
+        });
 
         it("keep in-block change", () => {
             const blocks: IBlock[] = [
@@ -148,9 +421,9 @@ describe("file-blocks-tracker", () => {
                 }
             ];
 
-            const actual = new FileBlocksTracker(blocks).splitChunks(chunks);
+            const actual = new FileBlocksTracker(blocks).applyChunks(chunks);
 
-            const expected: ILinesDelta[] = [
+            const expected: IBlockChange[] = [
                 {
                     startDelta: 0,
                     endDelta: 97
@@ -178,9 +451,9 @@ describe("file-blocks-tracker", () => {
                 }
             ];
 
-            const actual = new FileBlocksTracker(blocks).splitChunks(chunks);
+            const actual = new FileBlocksTracker(blocks).applyChunks(chunks);
 
-            const expected: ILinesDelta[] = [
+            const expected: IBlockChange[] = [
                 {
                     startDelta: 0,
                     endDelta: 0
@@ -208,14 +481,12 @@ describe("file-blocks-tracker", () => {
                 }
             ];
 
-            const actual = new FileBlocksTracker(blocks).splitChunks(chunks);
+            const actual = new FileBlocksTracker(blocks).applyChunks(chunks);
 
             expect(actual).toEqual([]);
         });
 
     })
-
-
 
 
     // it("adds lines to block", () => {
--- blocksProcessing/file-blocks-tracker.ts
+++ blocksProcessing/file-blocks-tracker.ts
@@ -3,14 +3,86 @@ import { IBlock } from "./block";
 interface IChunk {
     start: number;
     newStart: number;
-    deletedLines: string[],
-    addedLines: string[]
+    deletedLines: string[];
+    addedLines: string[];
 }
 
 interface IBlockChange {
     startDelta: number;
     endDelta: number;
 }
+
+function countOccurences(text: string, substring: string) {
+    return text.split(substring).length - 1;
+}
+
+function findNewBlockStart(deletedLines: string[], addedLines: string[], prevStartLineIndex: number) {
+    let currentLineIndex = addedLines.length - 1;
+
+    let balance = 1; // suppose that block has the last { on the line
+
+    if (prevStartLineIndex < deletedLines.length - 1) {
+        balance += deletedLines
+            .slice(prevStartLineIndex + 1)
+            .map(l => countOccurences(l, "{"))
+            .reduce((s, x) => s += x);
+    }
+
+    while (currentLineIndex >= 0) {
+        if (balance == 1 && hasBlockOpeningInTheEnd(addedLines[currentLineIndex])) {
+            return currentLineIndex;
+        }
+
+        const balanceDelta = countBlockBalance(addedLines[currentLineIndex]);
+        currentLineIndex -= 1;
+
+        balance -= balanceDelta;
+    }
+
+    return undefined;
+}
+
+function findNewBlockEnd(deletedLines: string[], addedLines: string[], prevEndLineIndex: number) {
+    let currentLineIndex = 0;
+
+    let balance = -1; // suppose that block has the first } on the line
+
+    if (prevEndLineIndex > 0) {
+        balance += deletedLines
+            .slice(0, prevEndLineIndex)
+            .map(l => countOccurences(l, "{"))
+            .reduce((s, x) => s += x);
+    }
+
+    while (currentLineIndex <= addedLines.length - 1) {
+        if (balance == -1 && hasBlockClosingInTheStart(addedLines[currentLineIndex])) {
+            return currentLineIndex;
+        }
+
+        const balanceDelta = countBlockBalance(addedLines[currentLineIndex]);
+        currentLineIndex += 1;
+
+        balance -= balanceDelta;
+    }
+
+    return undefined;
+}
+
+function hasBlockClosingInTheStart(line: string) {
+    const index = line.indexOf("}");
+    return index > -1 && (index < line.indexOf("{") || line.indexOf("{") === -1);
+}
+
+
+function hasBlockOpeningInTheEnd(line: string) {
+    const index = line.lastIndexOf("{");
+    return index > -1 && (index > line.lastIndexOf("}") || line.lastIndexOf("}") === -1);
+}
+
+function countBlockBalance(line: string) {
+    return countOccurences(line, "{") - countOccurences(line, "}");
+}
+
 
 class FileBlocksTracker {
     private _blocks: IBlock[];
@@ -19,7 +91,7 @@ class FileBlocksTracker {
         this._blocks = blocks.sort((a, b) => a.startLine - b.startLine);
     }
 
-    public splitChunks(chunks: IChunk[]): IBlockChange[] {
+    public applyChunks(chunks: IChunk[]): IBlockChange[] {
         const result: IBlockChange[] = [];
         let curBlockIndex = 0;
         let curBlock = this._blocks[curBlockIndex];
@@ -32,26 +104,14 @@ class FileBlocksTracker {
             if (chunk.start < curBlock.startLine) {
 
                 if (chunk.start + prevChunkLength - 1 >= curBlock.startLine) {
-                    const preBlockLength = curBlock.startLine - chunk.start
 
-                    const delta = newChunkLength - prevChunkLength;
+                    const newBlockStartline = chunk.start + findNewBlockStart(chunk.deletedLines, chunk.addedLines, curBlock.startLine - chunk.start);
+                    const startDelta = newBlockStartline - curBlock.startLine;
 
-                    throw new Error("Not implemented"); // detect moved block start
-
-                    // if (chunk.newLength > preBlockLength) {
-                    //     result.push(
-                    //         {
-                    //             start: chunk.start,
-                    //             delta: preBlockLength
-                    //         },
-                    //         {
-                    //             start: curBlock.startLine,
-                    //             delta
-                    //         }
-                    //     );
-                    // } else {
-                    //     // ??? block death ?
-                    // }
+                    result.push({
+                        startDelta,
+                        endDelta: newChunkLength - prevChunkLength - startDelta
+                    });
                 } else {
                     // [OK] ignore pre-block change
                 }
@@ -84,6 +144,9 @@ class FileBlocksTracker {
 
 export {
     IChunk,
-    IBlockChange as ILinesDelta,
-    FileBlocksTracker
+    IBlockChange,
+    FileBlocksTracker,
+    findNewBlockStart,
+    findNewBlockEnd,
+    countBlockBalance
 }
